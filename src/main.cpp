739b34895aaedcb3adced1550023998e3d3b5d7e
GuilhermeAQN <guilhermeaqn92@gmail.com>
2026-04-04 20:44:32 -0400

    feat: port Pong from VS2022 to VSCode with SDL2 and gamepad support

---
+++ src/main.cpp
@@ -0,0 +1,223 @@
+#include "raylib.h"
+
+#define XBOX_ALIAS_1 "xbox"
+#define XBOX_ALIAS_2 "x-box"
+#define PS_ALIAS_1   "playstation"
+#define PS_ALIAS_2   "sony"
+
+struct Game
+{
+	int player_score = 0;
+	int cpu_score = 0;
+	int gamepad = 0;
+	const float leftStickDeadzoneX = 0.1f;
+	const float leftStickDeadzoneY = 0.1f;
+	const float rightStickDeadzoneX = 0.1f;
+	const float rightStickDeadzoneY = 0.1f;
+	const float leftTriggerDeadzone = -0.9f;
+	const float rightTriggerDeadzone = -0.9f;
+
+	struct Ball
+	{
+		int x, y;
+		int speed_x, speed_y;
+		float radius;
+		float vibrationTimer;
+
+		void Draw() const
+		{
+			DrawCircle(x, y, radius, WHITE);
+		}
+
+		void Update(int &cpu_score, int &player_score, int &gamepad)
+		{
+			x += speed_x;
+			y += speed_y;
+
+			if (y + radius >= GetScreenHeight() || y - radius <= 0)
+			{
+				speed_y *= -1;
+			}
+			if (x + radius >= GetScreenWidth())
+			{
+				cpu_score++;
+				vibrationTimer = 0.3;
+				Reset();
+			}
+			if (x - radius <= 0)
+			{
+				player_score++;
+				vibrationTimer = 0.3;
+				Reset();
+			}
+		
+		}
+
+		void Reset()
+		{
+			x = GetScreenWidth() / 2;
+			y = GetScreenHeight() / 2;
+			speed_x = 7 * ((GetRandomValue(0, 1) == 0) ? -1 : 1);
+			speed_y = 7 * ((GetRandomValue(0, 1) == 0) ? -1 : 1);
+		}
+	};
+
+	struct Paddle
+	{
+		int x, y;
+		int width, height;
+		int speed;
+
+		void Draw() const
+		{
+			DrawRectangle(x, y, width, height, WHITE);
+		}
+
+		void Update()
+		{
+			if (IsKeyDown(KEY_UP) || IsGamepadButtonDown(0, GAMEPAD_BUTTON_LEFT_FACE_UP))
+			{
+				y -= speed;
+			}
+			if (IsKeyDown(KEY_DOWN) || IsGamepadButtonDown(0, GAMEPAD_BUTTON_LEFT_FACE_DOWN))
+			{
+				y += speed;
+			}
+			LimitMovement();
+		}
+
+		void LimitMovement()
+		{
+			if (y <= 10)
+			{
+				y = 5;
+			}
+			if (y + height >= GetScreenHeight())
+			{
+				y = GetScreenHeight() - height - 5;
+			}
+			// Clamp to valid range
+			if (y < 0)
+			{	
+				y = 0;
+			}
+			if (y + height > GetScreenHeight())
+			{
+				y = GetScreenHeight() - height;
+			}
+		}
+	};
+
+	struct CpuPaddle : Paddle
+	{
+		void Update(int ball_y)
+		{
+			if (y + height / 2 > ball_y)
+			{
+				y -= speed;
+			}
+			if (y + height / 2 < ball_y)
+			{
+				y += speed;
+			}
+			LimitMovement();
+		}
+	};
+
+	Ball ball{};
+	Paddle player{};
+	CpuPaddle cpuPaddle{};
+
+	void Init(int screen_width, int screen_height)
+	{
+		ball.radius = 20;
+		ball.Reset();
+
+		player.width = 25;
+		player.height = 120;
+		player.x = screen_width - player.width - 10;
+		player.y = screen_height / 2 - player.height / 2;
+		player.speed = 6;
+
+		cpuPaddle.width = 25;
+		cpuPaddle.height = 120;
+		cpuPaddle.x = 10;
+		cpuPaddle.y = screen_height / 2 - cpuPaddle.height / 2;
+		cpuPaddle.speed = 6;
+	}
+
+	void OnBallPlayerCollision()
+	{
+		// Push ball out of player paddle to prevent sticking
+		ball.x = player.x - (int)ball.radius;
+		ball.speed_x *= -1;
+	}
+
+	void OnBallCpuCollision()
+	{
+		// Push ball out of cpu paddle to prevent sticking
+		ball.x = cpuPaddle.x + cpuPaddle.width + (int)ball.radius;
+		ball.speed_x *= -1;
+	}
+
+	void Update()
+	{
+		ball.Update(cpu_score, player_score, gamepad);
+		player.Update();
+		cpuPaddle.Update(ball.y);
+
+		if (IsKeyPressed(KEY_LEFT) && gamepad > 0)
+			gamepad--;
+		if (CheckCollisionCircleRec(Vector2{(float)ball.x, (float)ball.y}, ball.radius, Rectangle{(float)player.x, (float)player.y, (float)player.width, (float)player.height}))
+		{
+			OnBallPlayerCollision();
+		}
+		if (CheckCollisionCircleRec(Vector2{(float)ball.x, (float)ball.y}, ball.radius, Rectangle{(float)cpuPaddle.x, (float)cpuPaddle.y, (float)cpuPaddle.width, (float)cpuPaddle.height}))
+		{
+			OnBallCpuCollision();
+		}
+
+		if (ball.vibrationTimer > 0)
+		{
+			SetGamepadVibration(0, 1.0, 1.0, ball.vibrationTimer);
+			ball.vibrationTimer -= GetFrameTime();
+		}
+	}
+
+	void Draw(int screen_width, int screen_height) const
+	{
+		ClearBackground(BLACK);
+		DrawLine(screen_width / 2, 0, screen_width / 2, screen_height, WHITE);
+		ball.Draw();
+		player.Draw();
+		cpuPaddle.Draw();
+		DrawText(TextFormat("%i", cpu_score), screen_width / 4 - 20, 20, 80, WHITE);
+		DrawText(TextFormat("%i", player_score), 3 * screen_width / 4 - 20, 20, 80, WHITE);
+	}
+};
+
+int main()
+{
+	const int screen_width = 1280;
+	const int screen_height = 800;
+	SetConfigFlags(FLAG_MSAA_4X_HINT);
+
+	InitWindow(screen_width, screen_height, "Pong Raylib Game");
+	SetTargetFPS(60);
+
+	Game game;
+	game.Init(screen_width, screen_height);
+
+	while (!WindowShouldClose())
+	{
+		
+		game.Update();
+
+		BeginDrawing();
+		game.Draw(screen_width, screen_height);
+		EndDrawing();
+	}
+
+	CloseWindow();
+	return 0;
+}
